d481977ee591f5a2b645478d14a5c42bbd338466
Fredrik Furtenbach <fredrik@Fredriks-MacBook.local>
2015-10-28 12:36:01 +0100

    1.4.2
    Handles UTF-8 in sender and location fields.
    
    Some code cleanup.

--- Beacon.indigoPlugin/Contents/Server plugin/plugin.py
+++ Beacon.indigoPlugin/Contents/Server plugin/plugin.py
@@ -65,19 +65,6 @@ class httpHandler(BaseHTTPRequestHandler):
       device.updateStateImageOnServer(indigo.kStateImageSel.MotionSensor)
       self.plugin.deviceList[device.id] = {'ref':device,'name':device.name,'address':device.address.lower()}
       return device.id
-
-   def sanityCheck(self,data,check):
-      self.plugin.debugLog(u"sanityCheck called")
-      try:
-         if all(name in data for name in check):
-            self.plugin.debugLog(u"Data passed sanityCheck")
-            return True
-         else:
-            self.plugin.debugLog(u"Data failed sanityCheck")
-            return False
-      except:
-         self.plugin.debugLog(u"Exception occured in sanityCheck")
-         return false
  
    def parseResult(self,sender,location,event):
       self.plugin.debugLog(u"parseResult called")
@@ -96,75 +83,65 @@ class httpHandler(BaseHTTPRequestHandler):
             newdev = self.deviceCreate(sender,location)
             self.deviceUpdate(self.plugin.deviceList[newdev]['ref'],deviceAddress,event)
 
-   def parseGeofancy(self,data):
-      self.plugin.debugLog(u"parseGeofancy called")
-      pdata = parse_qs(data)
-      p = {}
-      for key, value in pdata.iteritems():
-         p.update({key:value[0]})
-      if self.sanityCheck(p,self.plugin.geofancy_params):
-         self.parseResult(p["device"],p["id"],p["trigger"])
-
-   def parseGeohopper(self,data):
-      self.plugin.debugLog(u"parseGeohopper called")
-      p = json.loads(data)
-      if self.sanityCheck(p,self.plugin.geohopper_params):
-         self.parseResult(p["sender"],p["location"],p["event"])
-
-   def parseGeofency(self,data):
-      self.plugin.debugLog(u"parseGeofency called")
-      p = json.loads(data)
-      if self.sanityCheck(p,self.plugin.geofency_params):
-         self.parseResult(p["device"],p["name"],p["entry"])
-  
-   def parseBeecon(self,data):  
-      self.plugin.debugLog(u"parseBeecon called")
-      pdata = parse_qs(data)
-      p = {}
-      for key, value in pdata.iteritems():
-         p.update({key:value[0]})
-      if self.sanityCheck(p,self.plugin.beecon_params):
-         self.parseResult("Beecon",p["region"],p["action"])
-
    def do_POST(self):
       global rootnode
       foundDevice = False
       self.plugin.debugLog(u"Received HTTP POST")
-      try:
-         ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-         uagent = str(self.headers.getheader('user-agent'))
-         self.plugin.debugLog(u"User-agent: " + uagent)
-         if ('Geofancy' in uagent) and ctype == 'application/x-www-form-urlencoded' and self.plugin.geofancy:
-            data = self.rfile.read(int(self.headers['Content-Length']))
-            self.plugin.debugLog(u"Received Geofancy data: " + str(data))
-            self.parseGeofancy(data)
-         elif ('Geofency' in uagent) and ctype == 'application/json' and self.plugin.geofency:
-            data = self.rfile.read(int(self.headers['Content-Length']))
-            self.plugin.debugLog(u"Received Geofency data: " + str(data))
-            self.parseGeofency(data)
-         elif ('Beecon' in uagent) and self.plugin.beecon:
-            data = self.rfile.read(int(self.headers['Content-Length']))
-            self.plugin.debugLog(u"Received Beecon data: " + str(data))
-            self.parseBeecon(data)
-         elif ctype == 'application/json' and self.plugin.geohopper: 
-            data = self.rfile.read(int(self.headers['Content-Length']))
-            self.plugin.debugLog(u"Received JSON data: " + str(data))
-            self.parseGeohopper(data)
-      except:
-         pass
-         
       self.plugin.debugLog(u"Sending HTTP 200 response")
       self.send_response(200)
       self.end_headers()
+
+      try:
+         ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+         uagent = str(self.headers.getheader('user-agent'))
+         self.plugin.debugLog(u"User-agent: " + uagent)    
+         data = self.rfile.read(int(self.headers['Content-Length']))
+         data = data.decode('utf-8')         
+# Geofancy
+         if ('Geofancy' in uagent) and ctype == 'application/x-www-form-urlencoded' and self.plugin.geofancy:
+            self.plugin.debugLog(u"Received Geofancy data: " + data)
+            pdata = parse_qs(data)
+            p = {}
+            for key, value in pdata.iteritems():
+               p.update({key:value[0]})            
+            if all(name in data for name in ('device','id','trigger')):
+               self.parseResult(p["device"],p["id"],p["trigger"])
+            else:
+               self.plugin.debugLog(u"Missing parameters in received data")
+# Geofency
+         elif ('Geofency' in uagent) and ctype == 'application/json' and self.plugin.geofency:
+            self.plugin.debugLog(u"Received Geofency data: " + data)
+            p = json.loads(data)
+            if all(name in data for name in ('name','entry','device')):
+               self.parseResult(p["device"],p["name"],p["entry"])
+            else:
+               self.plugin.debugLog(u"Missing parameters in received data")
+#Beecon
+         elif ('Beecon' in uagent) and self.plugin.beecon:
+            self.plugin.debugLog(u"Received Beecon data: " + data)
+            pdata = parse_qs(data)
+            p = {}
+            for key, value in pdata.iteritems():
+               p.update({key:value[0]})
+            if all(name in data for name in ('region','action')):
+               self.parseResult("Beecon",p["region"],p["action"])
+            else:
+               self.plugin.debugLog(u"Missing parameters in received data")
+# Geohopper
+         elif ctype == 'application/json' and self.plugin.geohopper: 
+            self.plugin.debugLog(u"Received JSON data: " + data)
+            p = json.loads(data)
+            if all(name in data for name in ('sender','location','event')):
+               self.parseResult(p["sender"],p["location"],p["event"])
+            else:
+               self.plugin.debugLog(u"Missing parameters in received data")
+      except:
+         pass
 
 class Plugin(indigo.PluginBase):
    def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
       indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
       self.deviceList = {}
-      self.geohopper_params = ('sender','location','event')
-      self.geofancy_params = ('device','id','latitude','longitude','timestamp','trigger')
-      self.geofency_params = ('id','name','entry','date','latitude','longitude','device')
-      self.beecon_params = ('region','action')
       
       self.events = dict()
       self.events["stateChange"] = dict()
